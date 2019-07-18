4e7be57af67e15e36159a65f348aa97fe698a6b9
Meugen <meugenn.neg@gmail.com>
2019-07-18 21:14:06 +0300

    1

--- bot.py
+++ bot.py
@@ -1,8 +1,15 @@
 import telebot
+from telebot import types
 from const import token
 import localize
+from re import match
+import random
+from katakana import katakana
+from hiragana import hirogana
 
 bot = telebot.TeleBot(token)
+
+users_stat = {} #format user_id: {'answer': '', streak: 0}
 
 @bot.message_handler(commands=['start'])
 def start_handler(message):
@@ -10,6 +17,35 @@ def start_handler(message):
         lang = 'ru'
     else:
         lang = 'en'
-    bot.send_message(message.from_user.id, localize.start_message[lang])
+    markup = types.InlineKeyboardMarkup()
+    if lang == 'ru':
+        markup.row(
+            types.InlineKeyboardButton('Хирагана', callback_data='hiragana_ru'),
+            types.InlineKeyboardButton('Вперемешку', callback_data='mixed_0_ru'),
+            types.InlineKeyboardButton('Катакана', callback_data='katakana_ru'),
+        )
+    else:
+        markup.row(
+            types.InlineKeyboardButton('Hiragana', callback_data='hiragana_en'),
+            types.InlineKeyboardButton('Mixed', callback_data='mixed_0_en'),
+            types.InlineKeyboardButton('Katakana', callback_data='katakana_en'),
+        )
+    bot.send_message(message.from_user.id, localize.start_message[lang], reply_markup=markup)
+
+
+@bot.callback_query_handler(func=lambda call:match('mixed', call.data))
+def mixed(call):
+    t, streak, lang = call.data.split('_')
+    alph = random.choice(['h', 'k'])
+    if alph = 'h':
+        symb = random.choice(list(hirogana.items()))
+    else:
+        symb = random.choice(list(katakana.items()))
+    
+
+    bot.send_message()
+
+
+
 
 bot.polling(none_stop=True)
--- localize.py
+++ localize.py
@@ -1,4 +1,9 @@
 start_message = {
-    'en': 'Hi! I will help you learn japanese alphabet. Firstly, select: hiragana or katakana',
-    'ru': 'Привет! Я помогу тебе выучить японский алфавит. Для начала выбери его- катакану или хирагану'
+    'en': 'Hi! I will help you learn japanese alphabet. Firstly, select: hiragana, katakana or mixed up.',
+    'ru': 'Привет! Я помогу тебе выучить японский алфавит. Для начала выбери его- катакану, хирагану или вперемешку.'
+}
+
+back = {
+    'en': 'Back',
+    'ru': 'Назад'
 }
